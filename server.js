0ea6a774d55ef4b46f33abfcb928d47795c84b85
Philip Carlson <philip@cymmetry.com>
2015-09-20 12:37:16 -0700

    Generated initial project structure, with tweaks for Node, Express

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var express = require('express');
+
+// // TODO: will require differnt production environment settings (dist instead of src)
+// app.use('/favicon.ico', express.static(__dirname + '/src/favicon.ico'));
+// app.use(express.static(__dirname + '/src'));
+// app.use(express.static(__dirname + '/.tmp'));
+// app.use('/bower_components', express.static(__dirname + '/bower_components'));
